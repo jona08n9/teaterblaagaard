d0df465a308515b6790488a5b23868e94708a6e8
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2022-10-11 14:36:54 +0200

    noget rettelse

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", changeQuizImgBG);
+window.addEventListener("load", changeTitle);
 window.addEventListener("resize", changeQuizImgBG);
 
 function changeQuizImgBG() {
@@ -11,4 +12,15 @@ function changeQuizImgBG() {
     console.log("Vinduet er <756px");
     bgImage.srcset = "images/test-front.png";
   }
+}
+
+titleTimeout = setTimeout(changeTitle, 650);
+
+titleTimeout();
+
+function changeTitle() {
+  console.log("Skifter title");
+
+  const googleTitle = document.querySelector(".WidgetTitle__Header-sc-ruy1gu-2.jfpmQZ");
+  googleTitle.textContent = "Det siger tidligere besoegende";
 }
